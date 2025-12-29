e229da6de20ec4b4ed77fcf8c3e189cdbd950cfc
Au-venturous-Buddy <auventurous.buddy@gmail.com>
2025-12-29 18:42:24 +0800

    Initial Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.b78x.sia.gfea.auvb',
+  appName: "Au-venturous Buddy Gotta Fly 'Em All: Singapore Airlines B787-10",
+  webDir: 'public',
+  bundledWebRuntime: false
+};
+
+export default config;
---
+++ src/assets/index.md
@@ -0,0 +1,5 @@
+---
+title: "Singapore Airlines B787-10"
+retired: false
+category: "Wordpress Posts"
+---
